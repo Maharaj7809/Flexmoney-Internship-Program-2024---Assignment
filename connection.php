6c296b5271ed371706ba8d22ff7337ad0c630d51
Maharaj7809 <134138887+Maharaj7809@users.noreply.github.com>
2023-12-17 20:48:49 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,14 @@
 <?php
-$link = mysqli_connect("localhost","root","");
+
+
+$server="sql309.infinityfree.com";
+$username="	if0_35633454";
+$password="UsOq23uCV1yHfq";
+$dbname="if0_35633454_maharaj";
+
+
+
+$link = mysqli_connect($server ,$username ,$password , $dbname);
+
 mysqli_select_db($link,"yoga_firm");
 ?>
